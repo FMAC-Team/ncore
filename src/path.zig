5d19d4c9abc363c36802e36c04586b13150d034b
aqnya <siyueba73@gmail.com>
2026-03-21 17:24:33 +0800

    add loadkey.

--- src/path.zig
+++ src/path.zig
@@ -8,12 +8,16 @@ const jni = @cImport({
 });
 const allocator = std.heap.page_allocator;
 
+pub var app_context: jni.jobject = null;
+
 pub fn fetchPathFromSystem(env: *jni.JNIEnv) ![]u8 {
     const j = env.*.*;
 
     const at_class = j.FindClass.?(env, "android/app/ActivityThread") orelse return error.ClassNotFound;
     const get_app_mid = j.GetStaticMethodID.?(env, at_class, "currentApplication", "()Landroid/app/Application;") orelse return error.MethodNotFound;
     const context = j.CallStaticObjectMethod.?(env, at_class, get_app_mid) orelse return error.ContextNotFound;
+
+    app_context = j.NewGlobalRef.?(env, context);
 
     const ctx_class = j.FindClass.?(env, "android/content/Context") orelse return error.ClassNotFound;
     const get_files_mid = j.GetMethodID.?(env, ctx_class, "getFilesDir", "()Ljava/io/File;") orelse return error.MethodNotFound;
